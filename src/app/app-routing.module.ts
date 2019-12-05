d03ef19c951ab331f1222c5d28db9bb830a4317a
Ajay Marathe <ajay.marathe@xebecindia.com>
2019-12-05 16:10:07 +0530

    created sidebar and created all docs components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,52 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomeComponent} from "./home/home.component";
+/*docs*/
+import {OverviewComponent} from "./docs/overview/overview.component";
+import {AlertComponent} from "./docs/alert/alert.component";
+import {BadgeComponent} from "./docs/badge/badge.component";
+import {CardComponent} from "./docs/card/card.component";
+import {ButtonComponent} from "./docs/button/button.component";
+import {CarouselComponent} from "./docs/carousel/carousel.component";
+import {DropdownComponent} from "./docs/dropdown/dropdown.component";
+import {FormsComponent} from "./docs/forms/forms.component";
+import {ImagesComponent} from "./docs/images/images.component";
+import {LicenseComponent} from "./docs/license/license.component";
+import {ModalComponent} from "./docs/modal/modal.component";
+import {NavbarComponent} from "./docs/navbar/navbar.component";
+import {PaginationComponent} from "./docs/pagination/pagination.component";
+import {CloseComponent} from "./docs/close/close.component";
+import {PopoverComponent} from "./docs/popover/popover.component";
+import {ProgressComponent} from "./docs/progress/progress.component";
+import {SpinnerComponent} from "./docs/spinner/spinner.component";
+import {TabsComponent} from "./docs/tabs/tabs.component";
+import {TooltipComponent} from "./docs/tooltip/tooltip.component";
+import {TypographyComponent} from "./docs/typography/typography.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: OverviewComponent },
+  { path: 'alert', component: AlertComponent },
+  { path: 'badge', component: BadgeComponent },
+  { path: 'card', component: CardComponent },
+  { path: 'button', component: ButtonComponent },
+  { path: 'carousel', component: CarouselComponent },
+  { path: 'close_button', component: CloseComponent },
+  { path: 'dropdown', component: DropdownComponent },
+  { path: 'forms', component: FormsComponent },
+  { path: 'images', component: ImagesComponent },
+  { path: 'license', component: LicenseComponent },
+  { path: 'modal', component: ModalComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'pagination', component: PaginationComponent },
+  { path: 'popover', component: PopoverComponent },
+  { path: 'progress', component: ProgressComponent },
+  { path: 'spinner', component: SpinnerComponent },
+  { path: 'tabs', component: TabsComponent },
+  { path: 'tooltip', component: TooltipComponent },
+  { path: 'typography', component: TypographyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,106 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgFontawesomeModule } from 'ng-fontawesome-icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+/* custom components*/
+import { BaseNavbarComponent } from './components/base-navbar/base-navbar.component';
+import { BaseCardComponent } from './components/base-card/base-card.component';
+import { BaseCardBodyComponent } from './components/base-card/base-card-body/base-card-body.component';
+import { BaseCardHeaderComponent } from './components/base-card/base-card-header/base-card-header.component';
+import { BaseCardFooterComponent } from './components/base-card/base-card-footer/base-card-footer.component';
+import { BaseBadgeComponent } from './components/base-badge/base-badge.component';
+import { BaseButtonComponent } from './components/base-button/base-button.component';
+import { BaseDropdownComponent } from './components/base-dropdown/base-dropdown.component';
+import { BaseDropdownMenuComponent } from './components/base-dropdown/base-dropdown-menu/base-dropdown-menu.component';
+import { BaseCarouselComponent } from './components/base-carousel/base-carousel.component';
+import { BaseCarouselIndicatorComponent } from './components/base-carousel/base-carousel-indicator/base-carousel-indicator.component';
+import { BaseCarouselInnerComponent } from './components/base-carousel/base-carousel-inner/base-carousel-inner.component';
+import { BaseSlideIndicatorComponent } from './components/base-carousel/base-slide-indicator/base-slide-indicator.component';
+import { BaseCarouselCaptionComponent } from './components/base-carousel/base-carousel-caption/base-carousel-caption.component';
+import { BaseProgressComponent } from './components/base-progress/base-progress.component';
+import { BaseSpinnerComponent } from './components/base-spinner/base-spinner.component';
+import { BaseModalComponent } from './components/base-modal/base-modal.component';
+import { BaseModalHeaderComponent } from './components/base-modal/base-modal-header/base-modal-header.component';
+import { BaseModalBodyComponent } from './components/base-modal/base-modal-body/base-modal-body.component';
+import { BaseModalFooterComponent } from './components/base-modal/base-modal-footer/base-modal-footer.component';
+import { CloseButtonComponent } from './components/close-button/close-button.component';
+import { BaseAlertComponent } from './components/base-alert/base-alert.component';
+import { OverviewComponent } from './docs/overview/overview.component';
+import { LicenseComponent } from './docs/license/license.component';
+import { ButtonComponent } from './docs/button/button.component';
+import { AlertComponent } from './docs/alert/alert.component';
+import { BadgeComponent } from './docs/badge/badge.component';
+import { CardComponent } from './docs/card/card.component';
+import { CarouselComponent } from './docs/carousel/carousel.component';
+import { DropdownComponent } from './docs/dropdown/dropdown.component';
+import { ModalComponent } from './docs/modal/modal.component';
+import { NavbarComponent } from './docs/navbar/navbar.component';
+import { ProgressComponent } from './docs/progress/progress.component';
+import { SpinnerComponent } from './docs/spinner/spinner.component';
+import { PaginationComponent } from './docs/pagination/pagination.component';
+import { FormsComponent } from './docs/forms/forms.component';
+import { TooltipComponent } from './docs/tooltip/tooltip.component';
+import { PopoverComponent } from './docs/popover/popover.component';
+import { TabsComponent } from './docs/tabs/tabs.component';
+import { ImagesComponent } from './docs/images/images.component';
+import { TypographyComponent } from './docs/typography/typography.component';
+import { CloseComponent } from './docs/close/close.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BaseNavbarComponent,
+    BaseCardComponent,
+    BaseCardBodyComponent,
+    BaseCardHeaderComponent,
+    BaseCardFooterComponent,
+    BaseBadgeComponent,
+    BaseButtonComponent,
+    BaseDropdownComponent,
+    BaseDropdownMenuComponent,
+    BaseCarouselComponent,
+    BaseCarouselIndicatorComponent,
+    BaseCarouselInnerComponent,
+    BaseSlideIndicatorComponent,
+    BaseCarouselCaptionComponent,
+    BaseProgressComponent,
+    BaseSpinnerComponent,
+    BaseModalComponent,
+    BaseModalHeaderComponent,
+    BaseModalBodyComponent,
+    BaseModalFooterComponent,
+    CloseButtonComponent,
+    BaseAlertComponent,
+    OverviewComponent,
+    LicenseComponent,
+    ButtonComponent,
+    AlertComponent,
+    BadgeComponent,
+    CardComponent,
+    CarouselComponent,
+    DropdownComponent,
+    ModalComponent,
+    NavbarComponent,
+    ProgressComponent,
+    SpinnerComponent,
+    PaginationComponent,
+    FormsComponent,
+    TooltipComponent,
+    PopoverComponent,
+    TabsComponent,
+    ImagesComponent,
+    TypographyComponent,
+    CloseComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgFontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
